890501121749b2426fc04a9f1b9fdc0c7956bc89
vito <vitozhangyf@outlook.com>
2019-09-25 15:22:33 +0800

    remove test

--- TechnologyBBS.Server/test/app/controller/account.test.ts
+++ TechnologyBBS.Server/test/app/controller/account.test.ts
@@ -1,36 +1,36 @@
-import * as assert from 'assert';
-import { app } from 'egg-mock/bootstrap';
+// import * as assert from 'assert';
+// import { app } from 'egg-mock/bootstrap';
 
-describe('test/app/controller/api/account.test.ts', () => {
-  it('accout login', async () => {
-    const result = await app
-      .httpRequest()
-      .post('/api/v1/login')
-      .expect(200);
-    assert(result.body.code === 1);
-    assert(result.body.data === null);
-    assert(result.body.msg === '账号或密码必填');
+// describe('test/app/controller/api/account.test.ts', () => {
+//   it('accout login', async () => {
+//     const result = await app
+//       .httpRequest()
+//       .post('/api/v1/login')
+//       .expect(200);
+//     assert(result.body.code === 1);
+//     assert(result.body.data === null);
+//     assert(result.body.msg === '账号或密码必填');
 
-    const result1 = await app
-      .httpRequest()
-      .post('/api/v1/login')
-      .send({ user_name: 'vito', password: '123456' })
-      .expect(200);
-    assert(result1.body.code === 0);
-    assert(result1.body.data.user_name === 'vito');
-    assert(result1.body.msg === '登录成功');
-  });
+//     const result1 = await app
+//       .httpRequest()
+//       .post('/api/v1/login')
+//       .send({ user_name: 'vito', password: '123456' })
+//       .expect(200);
+//     assert(result1.body.code === 0);
+//     assert(result1.body.data.user_name === 'vito');
+//     assert(result1.body.msg === '登录成功');
+//   });
 
-  it('accout signin', async () => {
-    const result = await app
-      .httpRequest()
-      .post('/api/v1/signin')
-      .send({
-        user_name: 'vito',
-      })
-      .expect(200);
-    assert(result.body.code === 1);
-    assert(result.body.data === null);
-    assert(result.body.msg === '用户已存在');
-  });
-});
+//   it('accout signin', async () => {
+//     const result = await app
+//       .httpRequest()
+//       .post('/api/v1/signin')
+//       .send({
+//         user_name: 'vito',
+//       })
+//       .expect(200);
+//     assert(result.body.code === 1);
+//     assert(result.body.data === null);
+//     assert(result.body.msg === '用户已存在');
+//   });
+// });
--- TechnologyBBS.Server/test/app/service/account.test.ts
+++ TechnologyBBS.Server/test/app/service/account.test.ts
@@ -1,16 +1,16 @@
-import * as assert from 'assert';
-import { Context } from 'egg';
-import { app } from 'egg-mock/bootstrap';
+// import * as assert from 'assert';
+// import { Context } from 'egg';
+// import { app } from 'egg-mock/bootstrap';
 
-describe('test/app/service/account.test.js', () => {
-  let ctx: Context;
+// describe('test/app/service/account.test.js', () => {
+//   let ctx: Context;
 
-  before(async () => {
-    ctx = app.mockContext();
-  });
+//   before(async () => {
+//     ctx = app.mockContext();
+//   });
 
-  it('getUserByUserName vito', async () => {
-    const result = await ctx.service.account.getUserByUserName('vito');
-    assert(typeof result !== 'undefined');
-  });
-});
+//   it('getUserByUserName vito', async () => {
+//     const result = await ctx.service.account.getUserByUserName('vito');
+//     assert(typeof result !== 'undefined');
+//   });
+// });
